8c57e1ca6af6ccc9a7d8748702c4019b2d540e34
Andrey Kuzmin <kinjeiro@gmail.com>
2021-02-11 17:45:28 +0300

    feat(gql): add authMiddleware stub for apollo \ change uri to HttpLink

--- src/GraphQLWrapper.tsx
+++ src/GraphQLWrapper.tsx
@@ -1,17 +1,53 @@
 import React, { useEffect, useState } from 'react';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import {
+  ApolloClient, ApolloLink, ApolloProvider, concat, HttpLink, InMemoryCache,
+} from '@apollo/client';
 import { persistCache } from 'apollo3-cache-persist';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import AppLoading from 'expo-app-loading';
 
-const cache = new InMemoryCache();
+// todo @ANKU @CRIT @MAIN - вынести в конфиги
+const httpLink = new HttpLink({ uri: 'https://graphqlzero.almansi.me/api' });
+
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        project: {
+          merge: true,
+          // merge(existing, incoming) {
+          //  return incoming;
+          // },
+        },
+      },
+    },
+  },
+});
+
+const authMiddleware = new ApolloLink((operation, forward) => {
+  // add the authorization to the headers
+  // operation.setContext(({ headers = {} }) => ({
+  //  headers: {
+  //    ...headers,
+  //    authorization: localStorage.getItem('token') || null,
+  //  },
+  // }));
+  return forward(operation);
+});
 
 const client = new ApolloClient({
-  // todo @ANKU @CRIT @MAIN - вынести в конфиги
   // uri: 'https://api.graphql.guide/graphql',
-  uri: 'https://graphqlzero.almansi.me/api',
+  // uri: 'https://graphqlzero.almansi.me/api',
+  link: concat(authMiddleware, httpLink),
+
+  // todo @ANKU @LOW - Cache data may be lost when replacing the user field of a Query object.
   cache,
-  defaultOptions: { watchQuery: { fetchPolicy: 'cache-and-network' } },
+
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: 'cache-and-network',
+    },
+  },
 });
 
 export default function GraphQLWrapper({ children }: React.PropsWithChildren<any>) {
