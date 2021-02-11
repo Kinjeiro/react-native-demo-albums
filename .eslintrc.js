bafc9e34561f618fb5937d4372d111d7ab241728
Andrey Kuzmin <kinjeiro@gmail.com>
2021-02-11 17:54:11 +0300

    chore(eslint): change rules for comfortable work

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,11 +26,17 @@ module.exports = {
     'no-multiple-empty-lines': 0,
     'no-trailing-spaces': 0,
     'max-len': ['error', 120, 4, {'ignoreComments': true}],
+    'implicit-arrow-linebreak': 0,
+    'arrow-body-style': 0, // we have comments in empty function body
+
+    'react/no-unused-prop-types': 'warn',
 
     'react/jsx-indent-props': [2, 2],
     'react/jsx-boolean-value': ['error', 'always'],
     'react/jsx-curly-spacing': [2, 'always', {'spacing': { 'objectLiterals': 'never' }}], // spaces for jsx components props
+    'react/jsx-props-no-multi-spaces': 0,
 
     '@typescript-eslint/no-use-before-define': 0,
+    '@typescript-eslint/no-unused-vars': 'warn'
   },
 };
